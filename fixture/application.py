70d001f6a5ba99748339db3d91df5dd86cd308c5
Sandmir <sandmir2010@gmail.com>
2017-11-20 14:54:07 -0800

    pip

--- fixture/application.py
+++ fixture/application.py
@@ -8,13 +8,13 @@ class Application:
 
     def __init__(self):
 
-        app_dir = os.path.dirname(os.path.abspath(__file__)) + "/../support/com.udemy.android-4.0.4-APK4Fun.com.apk"
+        # app_dir = os.path.dirname(os.path.abspath(__file__)) + "/../support/com.udemy.android-4.0.4-APK4Fun.com.apk"
 
         capabilities = {
             'platformName': 'Android',
             'deviceName': 'emulator-5554',
             'platformVersion': '7.0',
-            'app': app_dir
+            'app':"/Privite/Study/Python/UdemyAppiumAndroid/support/com.udemy.android-4.0.4-APK4Fun.com.apk"
         }
 
         self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', capabilities)
